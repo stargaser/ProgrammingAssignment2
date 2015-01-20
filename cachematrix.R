d11738a64951652047f57b2204c5e90c619f6401
David Shupe <dave.shupe@gmail.com>
2015-01-20 12:41:15 -0800

    First version of cacheSolve function.

--- cachematrix.R
+++ cachematrix.R
@@ -21,10 +21,19 @@ makeCacheMatrix <- function(x = matrix()) {
     }
 
 
-## cacheSolve is a helper function that checks if the
+## cacheSolve is a function that checks if the
 ## inverse is cached. If yes, return the cached value.
 ## If no, compute and return the inverse while caching it too.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if (!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
